01ec0ca516ec0bc11460e28f2456d6aa1ddb8011
Ashish Kumar <helloashish20@gmail.com>
2025-07-15 16:57:43 +0530

    a

--- frontend/src/landing_page/about/Team.jsx
+++ frontend/src/landing_page/about/Team.jsx
@@ -14,14 +14,14 @@ function Team() {
         >
           <div className="col-6 p-3 text-center">
             <img 
-              src="media/images/nithinKamath.jpg" 
+              src="media/images/Ashish10001.jpg" 
               style={{borderRadius:"100%",width: "50%"}}
             />
-            <h4 className="mt-5">Nithin Kamath</h4>
+            <h4 className="mt-5">Ashish Kumar</h4>
             <h6>Founder , CEO</h6>
           </div>
           <div className="col-6 p-5">
-             <p>Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry. 
+             <p>Ashish bootstrapped and founded MyStock in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry. 
              </p>
              <p>
              He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC). 
